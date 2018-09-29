e16044891a73412d2087b8cc56a972101c59a64f
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2018-09-29 19:06:06 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,15 +17,31 @@ int main()
 
         if(turnCount % 2 == 0)
         {
-            std::cout << "Enter Row: ";
-            std::cin >> playerX;
-            std::cout << "Enter Column: ";
-            std::cin >> playerY;
+            bool valid_move_flag = false;
+            while(true)
+            {
+                std::cout << "Enter Row: ";
+                std::cin >> playerX;
+                std::cout << "Enter Column: ";
+                std::cin >> playerY;
+
+                valid_move_flag = (playerX >= 0) && (playerX <= 2) && (playerY >=0) && (playerY <=2) && (tttBoard[playerX][playerY].getChar() == '-');
+
+                if(valid_move_flag)
+                {
+                    break;
+                }
+                else
+                {
+                    std::cout << "Illegal move.\n";
+                }
+            }
+
             tttBoard[playerX][playerY].setChar('X');
         }
         else
         {
-            comp_minmax(tttBoard, turnCount, 1);
+            comp_minmax(tttBoard, turnCount);
         }
 
         winState = check_win(tttBoard);
